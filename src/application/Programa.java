a998f4b8a700daf1393267c2da3f68e630b915f0
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-07-18 17:43:23 -0300

    Reutilizando a instanciação

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -10,8 +10,8 @@ public class Programa {
 		
 		SellerDao sellerDao = DaoFactory.criarSellerDao();
 		
+		System.out.println("=== TEST 1: seller findById ====");
 		Seller seller = sellerDao.findById(3);
-		
 		System.out.println(seller);
 	}
 
