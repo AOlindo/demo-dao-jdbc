3334a0e54361b041840e3e9ec624c1d20d5b187a
Adriana Olindo <adriana-olindo18@hotmail.com>
2023-07-18 15:49:53 -0300

    VendedorDaoJDBC e DaoFactory

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.VendedorDaoo;
 import model.entities.Departamento;
 import model.entities.Vendedor;
 
@@ -12,6 +14,8 @@ public class Programa {
 		Departamento obj = new Departamento(1, "Livros");
 	
 		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		VendedorDaoo vendedorDao = DaoFactory.criarVendedorDaoo();
 		
 		System.out.println(vendedor);
 	}
